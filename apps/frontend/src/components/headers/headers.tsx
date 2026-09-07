6858ab3e7b7e919fab1f31ebc8b016ec777b463b
Denis <devorlich@gmail.com>
2026-09-07 19:26:31 +0300

    add go home button

--- apps/frontend/src/components/headers/headers.tsx
+++ apps/frontend/src/components/headers/headers.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
+import { FaHome } from 'react-icons/fa';
 
 import styles from './headers.module.scss';
 import { useAppDispatch, useAppSelector } from '@/hooks/useTypedRedux';
 import { toggleShowUserData } from '@/store/slices/ui';
 import { useGetMeQuery } from '@/api/rtk/user';
 import { UserIcon } from '@/components/icons and tags/icons';
+import Button from '@/components/buttons/buttons';
 
 interface IHeadersProp {
 	type: 'simple' | 'main';
@@ -32,6 +34,15 @@ const Headers: FC<IHeadersProp> = ({ type, className, style }) => {
 				style={style}
 				className={`${styles.main_header} ${className ?? ''}`}
 			>
+				<Button
+					to="/home"
+					variant="alternative"
+					className={styles.header_home}
+					aria-label="На главную"
+					title="На главную"
+				>
+					<FaHome />
+				</Button>
 				<div
 					style={{ opacity: showUserData ? 0 : 1 }}
 					className={styles.header_account}
